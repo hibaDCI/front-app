5edf4bcb5f3298b94f713e6bf46659ff6f4c05b1
mona <hiba.al-aani@digitalcareerinstitute.org>
2023-01-04 21:25:48 +0100

    modify axios

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,8 +1,23 @@
 import axios from "axios";
 
 const axiosApiInstance = axios.create();
+
 // axios.defaults.withCredentials = true;
-axiosApiInstance.defaults.baseURL = "https://superuser.onrender.com";
+
+console.log("node_env", process.env.NODE_ENV);
+
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000"
+    : "https://superuser.onrender.com";
+
+axiosApiInstance.defaults.baseURL = url;
 // axiosApiInstance.defaults.baseURL = "http://localhost:5000";
 
 export default axiosApiInstance;
+
+// how to specify when to use localhost and when to use the domain name
+
+// how to specify the type of the user if admin or guest
+
+// why after deployment the cookies doesn't work
